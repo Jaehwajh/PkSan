00fc17fd4ffd882ea000c26b595dfe9e6cf527b9
James Hwang <jaehwa.jh@gmail.com>
2022-07-06 15:36:12 -0700

    Updated Carousel and CSS

--- Assets/main.js
+++ Assets/main.js
@@ -17,3 +17,46 @@ function reveal(){
 }
 
 window.addEventListener("scroll", reveal);
+
+
+
+/* Carousel*/
+
+const slides = document.querySelectorAll('.slide')
+
+
+/*Next button*/
+const nextSlide = document.querySelector('#btn-next');
+
+let curSlide = 0;
+
+let maxSlide = slides.length - 1;
+
+nextSlide.addEventListener('click', function(){
+    if(curSlide === maxSlide){
+        curSlide = 0;
+    }else {
+        curSlide++;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+    })
+})
+
+
+/*Previous Button */
+
+const prevSlide = document.querySelector('#btn-prev')
+
+prevSlide.addEventListener('click', function (){
+    if (curSlide === 0){
+        curSlide = maxSlide;
+    }else {
+        curSlide--;
+    }
+
+    slides.forEach((slide,index) => {
+        slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+    })
+})
